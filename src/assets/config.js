9eacee7e8da850b8372b463f1848f23db255bded
qirenji <q290187113@qq.com>
2018-08-20 21:12:09 +0800

    build

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,5 +1,6 @@
 // 网络请求地址
-export const URL = 'http://localhost:3000';
+// export const URL = 'http://localhost:3000';
+export const URL = '';
 
 export const hotKeywords = ['张杰', '赵雷', '李健', '林志炫', '张碧晨', '梁博', '周笔畅', '张靓颖', '陈奕迅', '周杰伦', '王力宏', 'TFBoys', '李玉刚', '魏晨', '薛之谦'];
 
